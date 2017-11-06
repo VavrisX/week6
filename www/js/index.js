a071f01d2f6ccf463b12d579a281fdefc2a0f542
VavrisX <vavl1_16@uni.worc.ac.uk>
2017-11-06 11:08:00 +0000

    end

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,14 @@
-$(document).on("pagecreate","#pageone",function(){
-  $('#submitButton').on("click", function(){
-    submitText();
-  });            
-});            
-
-
+    
 function submitText() {
 	var text = $('#textinput').val();
 	alert(text);
+    storeValue();
 }
 
 function storeValue(key, value) {
-	//add some code to store the key-value pair in persistant storage 
+   localStorage.setItem('userInput', $('#textinput').val());
+}
+
+function outputValue(){
+   alert( "User input is = " + localStorage.getItem("userInput"));
 }
